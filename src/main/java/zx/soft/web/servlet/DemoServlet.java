d1267ccc9f80d5c9e2b56ab4e19274fa7dd3fddd
蔡菡泠 <caihanling@zxisl.com>
2015-09-23 15:03:17 +0800

    解决跨域访问

--- src/main/java/zx/soft/web/servlet/DemoServlet.java
+++ src/main/java/zx/soft/web/servlet/DemoServlet.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-/**
- * Created by jimbo on 9/17/15.
- */
 public class DemoServlet extends HttpServlet {
+	
+	GetInterface getInterface = new GetInterface();
 
-    GetInterface getInterface = new GetInterface();
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        response.setCharacterEncoding("UTF-8");
-        PrintWriter writer = response.getWriter();
-        Object result = getInterface.getData("http://192.168.6.200:8080/apt-api/");
-        writer.print(result);
-    }
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setCharacterEncoding("UTF-8");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		PrintWriter writer = response.getWriter();
+		Object result = getInterface.getData("http://localhost:8080/rpc-api/");
+		writer.print(result);
+	}
+
 }
